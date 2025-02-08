be8ed39eb28653ef3d4464fd5ae76c82f3f179dc
pedroHaname67Br <31941740+pedroHaname67Br@users.noreply.github.com>
2025-02-08 23:21:15 +0000

    Criei um sistema de entrada e saída de Dados.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,42 @@
 //Teste larissa
 
 int main() {
+    // Variáveis
+    char nome;
+    int codigo_da_cidade;
+    int populacao;
+    float area;
+    float PIB;
+    int numeros_de_pontos_turisticos;
+    
+    // Entrada de Dados
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s",&nome);
+
+    printf("Digite o Código da Cidade: \n");
+    scanf("%d",&codigo_da_cidade);
+
+    printf("Digite o número da população: \n");
+    scanf("%d",&populacao);
+
+    printf("Digite a área da Cidade: \n");
+    scanf("%f",&area);
+
+    printf("Digite o PIB da Cidade: \n");
+    scanf("%f",&PIB);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d",&numeros_de_pontos_turisticos);
+
+    // Saída de Dados
+    printf("Nome da Cidade: %s\n",nome);
+    printf("Código da Cidade: %d\n",codigo_da_cidade);
+    printf("População da Cidade: %d\n",populacao);
+    printf("Área da Cidade: %f\n",area);
+    printf("PIB da Cidade: %f\n",PIB);
+    printf("Quantidade de Pontos Turísticos: %d\n",numeros_de_pontos_turisticos);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
